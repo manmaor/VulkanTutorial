e55750368197a8dfaa87c21cf8e41561cd17ed1a
unknown <manmaor2@gmail.com>
2024-12-27 17:37:18 +0200

    load props

--- src/main/kotlin/engine/EngineProperties.kt
+++ src/main/kotlin/engine/EngineProperties.kt
@@ -1,16 +1,16 @@
 package com.maorbarak.engine
 
 object EngineProperties {
-    private const val defaultUPS = 30
-    private const val defaultValidate = false
-    private const val filename = "eng.properties"
+    private const val DEFAULT_UPS = 30
+    private const val DEFAULT_VALIDATE = false
+    private const val FILENAME = "eng.properties"
 
     val ups: Int
     val validate: Boolean
 
     init {
         // Reading properties file
-        EngineProperties.javaClass.getResourceAsStream("/$filename").use { stream ->
+        EngineProperties.javaClass.getResourceAsStream("/$FILENAME").use { stream ->
             val props: Map<String, String> = stream?.reader()
                 ?.readLines()
                 ?.filter { it.contains("=") }
@@ -20,8 +20,8 @@ object EngineProperties {
                         .split("=")
                         .let { it[0] to it[1] }
                 } ?: mapOf()
-            ups = props.getOrDefault("ups", defaultUPS).toString().toInt()
-            validate = props.getOrDefault("validate", defaultValidate).toString().toBoolean()
+            ups = props.getOrDefault("ups", DEFAULT_UPS).toString().toInt()
+            validate = props.getOrDefault("validate", DEFAULT_VALIDATE).toString().toBoolean()
         }
     }
 }
